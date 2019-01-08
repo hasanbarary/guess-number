ec7fccaec4c809d42640957b015fffb23e457902
hasanbarary <46402575+hasanbarary@users.noreply.github.com>
2019-01-08 12:41:42 +0300

    edit message for user

--- numbers.py
+++ numbers.py
@@ -2,20 +2,19 @@
 from random import randint
 def random():
     rand_number = randint(0, 100)
-    print(rand_number)
     return rand_number
 rand_number=random()
 answer = 'yes'
 while answer == 'yes':
     user_number=int(input('enter your guess number --> '))
     if user_number < rand_number:
-        print("Your guess ('{}') lower Than My number ".format(user_number))
+        print("Your guess ('{}') lower Than My number.Enter a larger Numeber ".format(user_number))
     elif user_number > rand_number:
-        print("Your guess ('{}')  greater Than My number ".format(user_number))
+        print("Your guess ('{}')  greater Than My number.Enter a lower number  ".format(user_number))
     else:
         print("Cngrulation You are win".format(user_number))
         while True:
-           answer = input("Do you want Play Game agin (Please Enter Yes/No) --> ").lower()
+           answer = input("Do you want Play Game agin (Please just Enter Yes/No) --> ").lower()
            if answer == 'yes':
                rand_number=random()
                break
