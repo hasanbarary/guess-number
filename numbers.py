a76590a5df21a946489b4299feef23e9e2de700f
hasanbarary <46402575+hasanbarary@users.noreply.github.com>
2019-01-08 13:16:34 +0300

    I Add attempt count for user to know it
    guess_attempt is this variable

--- numbers.py
+++ numbers.py
@@ -4,15 +4,18 @@ def random():
     rand_number = randint(0, 100)
     return rand_number
 rand_number=random()
+guess_attempt=0
 answer = 'yes'
 while answer == 'yes':
     user_number=int(input('enter your guess number --> '))
+    guess_attempt+=1 #this variables for attempt count
     if user_number < rand_number:
-        print("Your guess ('{}') lower Than My number.Enter a larger Numeber ".format(user_number))
+        print("Your guess ('{}') lower Than My number.Enter a larger Numeber --> ".format(user_number))
     elif user_number > rand_number:
-        print("Your guess ('{}')  greater Than My number.Enter a lower number  ".format(user_number))
+        print("Your guess ('{}')  greater Than My number.Enter a lower number --> ".format(user_number))
     else:
-        print("Cngrulation You are win".format(user_number))
+        print("congratulations :). You are win You guessed your {} attempt.".format(guess_attempt))
+        guess_attempt=0
         while True:
            answer = input("Do you want Play Game agin (Please just Enter Yes/No) --> ").lower()
            if answer == 'yes':
@@ -23,4 +26,3 @@ while answer == 'yes':
                break
            else:
                continue
-
