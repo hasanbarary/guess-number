c7e5c7a67212d4141abe046be96663c8cde700e7
hasanbarary <46402575+hasanbarary@users.noreply.github.com>
2019-01-15 19:18:36 +0300

    add level function and life count

--- numbers.py
+++ numbers.py
@@ -1,20 +1,42 @@
 #!/usr/bin/python3.6
 from random import randint
+def geme_level():
+    try:
+        print("Choise Game Level  >>>\n 1.EASY \n 2.NORMAL \n 3.HARD")
+        answer=int(input(">>> "))
+    except:
+        geme_level()
+    if answer == 1:
+        return 10
+    elif answer == 2:
+        return 7
+    else:
+        return 5
 def random():
     rand_number = randint(0, 100)
     return rand_number
 rand_number=random()
 guess_attempt=0
 answer = 'yes'
+level=int(geme_level())
+print(level)
 while answer == 'yes':
-    user_number=int(input('enter your guess number --> '))
+    while True:
+        try:
+            user_number=eval(input('enter your guess number >>> '))
+            break
+        except:
+            print("Please enter Number for this Game Try")
+            continue
     guess_attempt+=1 #this variables for attempt count
-    if (user_number < rand_number) & (guess_attempt < 5):
+    if (user_number < rand_number) & (guess_attempt < level):
         print("Your guess ('{}') lower Than My number.Enter a larger Numeber --> ".format(user_number))
-    elif (user_number > rand_number) & (guess_attempt < 5):
+        print("Your Lif {}/{}".format(level-guess_attempt,level))
+    elif (user_number > rand_number) & (guess_attempt < level):
         print("Your guess ('{}')  greater Than My number.Enter a lower number --> ".format(user_number))
+        print("Your Lif {}/{}".format(level - guess_attempt, level))
     else:
-        if guess_attempt == 5:
+        if guess_attempt == level:
             print("You are Lose  :(. Dont Wory. Try agin.You Win next Try".format(guess_attempt))
         else:
             print("congratulations :). You are win You guessed your {} attempt.".format(guess_attempt))
